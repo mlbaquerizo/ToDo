64e463ffbe9346dbc2103608d0c5ce345e448c6f
mlbaquerizo <mlbaquerizo47@gmail.com>
2016-11-07 10:02:44 -0500

    Begin building command line args to show to do list

--- App.java
+++ App.java
@@ -1,15 +1,25 @@
 class App {
 	public static void main(String[] args) {
+		String message;
 		String[] itemArray = {"buy groceries", "walk the dog"};
 		ToDoList myToDoList = new ToDoList("My ToDo", itemArray);
 		ListController myController = new ListController(myToDoList);
 
 		myController.addItem("eat dinner");
-		myController.showList("fdsa");
-		myController.deleteItem(3);
-		myController.showList("full");
-		myController.showList("complete");
-		myController.showList("pending");
+
+		if(args.length > 0){
+			switch (args[0]){
+				case "full": myController.showList("full");
+					break;
+				case "complete": myController.showList("complete");
+					break;
+				case "pending": myController. showList("pending");
+					break;	
+				default: System.out.println("Commands: full, complete, pending");
+			}
+		} else {
+			System.out.print("Please use commands: full, complete, pending");
+		}
 
 		// TODO: Allow user input. Program start brings up "What do you want to do?" dialogue
 	}
